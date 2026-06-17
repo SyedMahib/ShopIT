0ddd80d2526fb61c167f54edce3b63c5f61478da
SyedMahib <syedmahib8@gmail.com>
2026-06-17 13:35:30 +0600

    addeda config file and also created an app.js file for the backend

---
+++ Backend/app.js
@@ -0,0 +1,9 @@
+import express from "express";
+const app = express();
+
+import dotenv from "dotenv";
+dotenv.config({ path:"Backend/config/config.env" });
+
+app.listen(process.env.PORT, () => {
+    console.log(`server is running on port: ${process.env.PORT}`);
+});  
