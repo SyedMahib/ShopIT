d3ce839ab03bd5031b123f29f305f5004a6de569
SyedMahib <syedmahib8@gmail.com>
2026-06-17 13:54:10 +0600

    added development and production variables for the server

--- Backend/app.js
+++ Backend/app.js
@@ -5,5 +5,5 @@ import dotenv from "dotenv";
 dotenv.config({ path:"Backend/config/config.env" });
 
 app.listen(process.env.PORT, () => {
-    console.log(`server is running on port: ${process.env.PORT}`);
+    console.log(`server is running on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });  
