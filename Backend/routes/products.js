0f08e0f20e79406bfb2cb3fd54edb6d4e5e89192
SyedMahib <syedmahib8@gmail.com>
2026-06-18 14:41:11 +0600

    created API for the delete product

--- Backend/routes/products.js
+++ Backend/routes/products.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getProductDetails, getProducts, newProduct, updateProduct } from '../controllers/productControllers.js';
+import { deleteProduct, getProductDetails, getProducts, newProduct, updateProduct } from '../controllers/productControllers.js';
 const router = express.Router();
 
 
@@ -8,5 +8,6 @@ router.route('/admin/products').post(newProduct);
 
 router.route('/products/:id').get(getProductDetails);
 router.route('/products/:id').put(updateProduct);
+router.route('/products/:id').delete(deleteProduct);
 
 export default router;
