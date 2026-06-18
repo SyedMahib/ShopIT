a3512a672f5548671bb0f8e5b4457aadd4a3711e
SyedMahib <syedmahib8@gmail.com>
2026-06-18 11:24:24 +0600

    added 2 folders for api routes like controllers and products

--- Backend/app.js
+++ Backend/app.js
@@ -4,6 +4,13 @@ const app = express();
 import dotenv from "dotenv";
 dotenv.config({ path:"Backend/config/config.env" });
 
+
+// import all routes
+
+import productRoutes from './routes/products.js';
+
+app.use('/api/v1', productRoutes);
+
 app.listen(process.env.PORT, () => {
     console.log(`server is running on port: ${process.env.PORT} in ${process.env.NODE_ENV} mode`);
 });  
---
+++ Backend/controllers/productControllers.js
@@ -0,0 +1,7 @@
+
+
+export const getProducts = (req, res) => {
+    res.status(200).json({ 
+        message: 'Gett all products',
+    });
+};
