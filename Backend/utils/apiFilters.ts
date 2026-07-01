771a9281715911dedb5daa21b8110fc21c63b996
SyedMahib <syedmahib8@gmail.com>
2026-07-01 11:39:17 +0600

    updated some syntaxes

--- Backend/utils/apiFilters.ts
+++ Backend/utils/apiFilters.ts
@@ -1,51 +1,50 @@
 import { Query } from "mongoose";
 
-
 class APIFilters {
-    query: Query<any[], any>;
-    queryStr: Record<string, string>;
+  query: Query<any[], any>;
+  queryStr: Record<string, string>;
 
-    constructor (query: Query<any[], any>, queryStr: Record<string, string>) {
-        this.query = query;
-        this.queryStr = queryStr;
-    }
+  constructor(query: Query<any[], any>, queryStr: Record<string, string>) {
+    this.query = query;
+    this.queryStr = queryStr;
+  }
 
+  search() {
+    const keyword = this.queryStr.keyword
+      ? {
+          name: {
+            $regex: this.queryStr.keyword,
+            $options: "i",
+          },
+        }
+      : {};
 
-    search() {
-        const keyword = this.queryStr.keyword ? {
-            name: {
-                $regex: this.queryStr.keyword,
-                $options: "i",
-            },
-        } : {};
+    this.query = this.query.find({ ...keyword });
+    return this;
+  }
 
-        this.query = this.query.find({ ...keyword });
-        return this;
-    };
+  filters() {
+    const queryCopy = { ...this.queryStr };
 
-    filters() {
-        const queryCopy = { ...this.queryStr };
+    // Fields to remove from the query string
+    const fieldsToRemove = ["Keyword", "page"];
+    fieldsToRemove.forEach((el) => delete queryCopy[el]);
 
-        // Fields to remove from the query string
-        const fieldsToRemove = ["Keyword", "page"];
-        fieldsToRemove.forEach((el) => delete queryCopy[el]);
+    // Advance filter for price, rating, etc.
+    let queryStr = JSON.stringify(queryCopy);
+    queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
 
-        // Advance filter for price, rating, etc.
-        let queryStr = JSON.stringify(queryCopy);
-        queryStr = queryStr.replace(/\b(gt|gte|lt|lte)\b/g, (match) => `$${match}`);
+    this.query = this.query.find(JSON.parse(queryStr));
+    return this;
+  }
 
-        this.query = this.query.find(JSON.parse(queryStr));
-        return this;
-    };
+  pagination(resPerPage: number) {
+    const currentPage = Number(this.queryStr.page) || 1;
+    const skip = resPerPage * (currentPage - 1);
 
-    pagination(resPerPage: number) {
-        const currentPage = Number(this.queryStr.page) || 1;
-        const skip = resPerPage * (currentPage - 1);
-
-        this.query = this.query.limit(resPerPage).skip(skip);
-        return this;
-    }
-};
-
+    this.query = this.query.limit(resPerPage).skip(skip);
+    return this;
+  }
+}
 
 export default APIFilters;
